8308e8eeee2ba067591f5915834501d4a636de17
Mayank <mayank.singh@renewbuy.com>
2017-04-30 22:45:30 +0530

    Url Related Change

--- htmlcloud/htmlcloud/urls.py
+++ htmlcloud/htmlcloud/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^assignment/', include('assignment.urls')),
+    url(r'^', include('assignment.urls')),
     url(r'^admin/', admin.site.urls),
 ]
